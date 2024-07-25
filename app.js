86260037edc3d8a412b7fead2c44be64018c1ea8
j1909omae <143763689+j1909omae@users.noreply.github.com>
2024-07-25 08:33:22 +0000

    課題アプリにログインを追加

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cookieSession = require("cookie-session");
+const secret = "secretCuisine123";
 
-var app = express();
+const app = express();
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [secret],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require("./routes"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
